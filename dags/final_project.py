026e277c9fdbfd4347494b7687024f5de8024005
Natasha Estrada <nestra5@wgu.edu>
2025-11-20 11:13:25 -0500

    Added start/end operators to test DAG

--- dags/final_project.py
+++ dags/final_project.py
@@ -1,26 +1,31 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 import pendulum
-import os
 from airflow.decorators import dag
 from airflow.operators.dummy import DummyOperator
-from operators import (StageToRedshiftOperator, LoadFactOperator,
-                       LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
+from operators import StageToRedshiftOperator
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': pendulum.now(),
+    "owner": "udacity",
+    "depends_on_past": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+    "email_on_retry": False,
+    "start_date": pendulum.datetime(2023, 1, 1),
 }
 
 @dag(
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *'
+    schedule_interval='0 * * * *',
+    catchup=False
 )
 def final_project():
 
+    # Start / End
     start_operator = DummyOperator(task_id='Begin_execution')
+    end_operator = DummyOperator(task_id='Stop_execution')
 
+    # Stage_events only
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id="Stage_events",
         redshift_conn_id="redshift",
@@ -30,32 +35,8 @@ def final_project():
         s3_key="log-data",
         json_path="s3://airflow-project-nestr/log_json_path.json",
     )
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs',
-    )
 
-    load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table',
-    )
+    start_operator >> stage_events_to_redshift >> end_operator
 
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table',
-    )
-
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table',
-    )
-
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table',
-    )
-
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table',
-    )
-
-    run_quality_checks = DataQualityOperator(
-        task_id='Run_data_quality_checks',
-    )
 
 final_project_dag = final_project()
