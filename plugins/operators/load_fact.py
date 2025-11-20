dfd73cd9edf6692914278bebc1359d904a9770b0
Natasha Estrada <nestra5@wgu.edu>
2025-11-20 12:49:48 -0500

    Edit load_fact.py to resolve issue

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,33 +2,42 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = "#F98866"
 
     @apply_defaults
-    def __init__(self,
-                 redshift_conn_id="redshift",
-                 table="",
-                 sql_statement="",
-                 *args, **kwargs):
+    def __init__(
+        self,
+        redshift_conn_id="",
+        table="",
+        sql="",
+        *args,
+        **kwargs
+    ):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
-        self.sql_statement = sql_statement
+        self.sql = sql
 
     def execute(self, context):
-        self.log.info(f"Loading fact table: {self.table}")
+        self.log.info(f"Starting load into fact table `{self.table}`")
 
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        # Build final SQL
         insert_sql = f"""
             INSERT INTO {self.table}
-            {self.sql_statement};
+            {self.sql}
         """
 
-        self.log.info(f"Executing INSERT statement for {self.table}")
+        self.log.info("Executing fact table load SQL:")
+        self.log.info(insert_sql)
+
+        # Run the INSERT command
         redshift.run(insert_sql)
 
-        self.log.info(f"Fact table {self.table} loaded successfully.")
+        self.log.info(f"Fact table `{self.table}` successfully loaded.")
+
